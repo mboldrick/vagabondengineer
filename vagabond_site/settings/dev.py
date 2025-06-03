10fefef3b494dce0e5c9e1a62ac714a2c24b0139
Mike Boldrick <mb@boldrick.net>
2025-06-02 20:07:32 -0700

    Initial commit for vagabond_site project with Django setup, including base settings, development and production configurations, static files, templates for error handling, and Wagtail integration.

---
+++ vagabond_site/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-0j5m462v56wguq^nb-wt+nxpwy@!rqjjm3@q3z^hk-4hf$2ozm"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
