b22ebd7b4fd75d9ced7fccd728cbf2089e309f9a
Mike Boldrick <mb@boldrick.net>
2025-06-07 16:02:04 -0700

    Refactor HomePage model and template to include hero fields and about me section

--- home/models.py
+++ home/models.py
@@ -2,9 +2,25 @@ from django.db import models
 
 from wagtail.models import Page
 from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
 
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
+    hero_heading = models.CharField(max_length=100, default="")
+    hero_tagline = models.TextField(default="")
+    hero_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        verbose_name="Hero Image",
+    )
+    about_me = RichTextField(blank=True)
 
-    content_panels = Page.content_panels + ["body"]
+    content_panels = Page.content_panels + [
+        FieldPanel("hero_heading"),
+        FieldPanel("hero_tagline"),
+        FieldPanel("hero_image"),
+        FieldPanel("about_me"),
+    ]
