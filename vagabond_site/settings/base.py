6be14a589edbcb209c01c798a2be1ce648558ddd
Mike Boldrick <mb@boldrick.net>
2025-06-08 07:52:14 -0700

    Add Tailwind CSS integration and update base template styles
    - Introduced Tailwind CSS by creating styles.css and importing it in base.html.
    - Updated home_page.html to apply new Tailwind styles to hero heading.
    - Modified .gitignore to exclude Tailwind build outputs.
    - Adjusted STATICFILES_DIRS and STATIC_ROOT in settings for better organization.

--- vagabond_site/settings/base.py
+++ vagabond_site/settings/base.py
@@ -133,10 +133,10 @@ STATICFILES_FINDERS = [
 ]
 
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, "static"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -182,4 +182,15 @@ WAGTAILADMIN_BASE_URL = "http://example.com"
 # This can be omitted to allow all files, but note that this may present a security risk
 # if untrusted users are allowed to upload files -
 # see https://docs.wagtail.org/en/stable/advanced_topics/deploying.html#user-uploaded-files
-WAGTAILDOCS_EXTENSIONS = ['csv', 'docx', 'key', 'odt', 'pdf', 'pptx', 'rtf', 'txt', 'xlsx', 'zip']
+WAGTAILDOCS_EXTENSIONS = [
+    "csv",
+    "docx",
+    "key",
+    "odt",
+    "pdf",
+    "pptx",
+    "rtf",
+    "txt",
+    "xlsx",
+    "zip",
+]
