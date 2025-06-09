74c7a548b037f097bdc28503859fe365ed0e220c
Mike Boldrick <mb@boldrick.net>
2025-06-09 16:26:52 -0700

    Add initial project setup with Tailwind CSS and update base templates
    - Created package.json for project dependencies and scripts.
    - Updated styles.css to include custom properties and utility classes for Tailwind CSS.
    - Modified base.py to adjust STATIC_URL and add additional STATICFILES_DIRS.
    - Enhanced base.html to include Google Fonts and structured content blocks for better styling.

--- vagabond_site/settings/base.py
+++ vagabond_site/settings/base.py
@@ -134,10 +134,11 @@ STATICFILES_FINDERS = [
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    "/var/www/static/",
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
