73296eac95f5f91c858aaedb03e900acd3e1fdd8
Mike Boldrick <mb@boldrick.net>
2025-06-09 17:55:34 -0700

    Add NavigationSettings model and update settings for Wagtail integration

--- base/models.py
+++ base/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from wagtail.admin.panels import (
+    FieldPanel,
+    MultiFieldPanel,
+)
+from wagtail.contrib.settings.models import (
+    BaseGenericSetting,
+    register_setting,
+)
 
-# Create your models here.
+
+@register_setting
+class NavigationSettings(BaseGenericSetting):
+    linkedin_url = models.URLField(verbose_name="LinkedIn URL", blank=True)
+    github_url = models.URLField(verbose_name="GitHub URL", blank=True)
+    mastodon_url = models.URLField(verbose_name="Mastodon URL", blank=True)
+    twitter_url = models.URLField(verbose_name="X (Twitter) URL", blank=True)
+    instagram_url = models.URLField(verbose_name="Instagram URL", blank=True)
+
+    panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel("linkedin_url"),
+                FieldPanel("github_url"),
+                FieldPanel("mastodon_url"),
+                FieldPanel("twitter_url"),
+                FieldPanel("instagram_url"),
+            ],
+            "Social settings",
+        )
+    ]
