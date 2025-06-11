9f18a1ecd7631ea7cc88926394f6ee3640b48f5e
Mike Boldrick <mb@boldrick.net>
2025-06-11 10:03:52 -0700

    Add form handling models and templates for email forms, and clean up unused CSS classes

--- base/models.py
+++ base/models.py
@@ -1,24 +1,30 @@
 from django.db import models
+
+from modelcluster.fields import ParentalKey
+
 from wagtail.admin.panels import (
     FieldPanel,
+    FieldRowPanel,
+    InlinePanel,
     MultiFieldPanel,
     PublishingPanel,
 )
-
 from wagtail.fields import RichTextField
-
 from wagtail.models import (
     DraftStateMixin,
     PreviewableMixin,
     RevisionMixin,
     TranslatableMixin,
 )
-
+from wagtail.contrib.forms.models import (
+    AbstractEmailForm,
+    AbstractFormField,
+)
+from wagtail.contrib.forms.panels import FormSubmissionsPanel
 from wagtail.contrib.settings.models import (
     BaseGenericSetting,
     register_setting,
 )
-
 from wagtail.snippets.models import register_snippet
 
 
@@ -70,3 +76,31 @@ class FooterText(
 
     class Meta(TranslatableMixin.Meta):
         verbose_name_plural = "Footer Text"
+
+
+class FormField(AbstractFormField):
+    page = ParentalKey("FormPage", on_delete=models.CASCADE, related_name="form_fields")
+
+
+class FormPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FormSubmissionsPanel(),
+        FieldPanel("intro"),
+        InlinePanel("form_fields", label="Form fields"),
+        FieldPanel("thank_you_text"),
+        MultiFieldPanel(
+            [
+                FieldRowPanel(
+                    [
+                        FieldPanel("to_address"),
+                        FieldPanel("from_address"),
+                    ]
+                ),
+                FieldPanel("subject"),
+            ],
+            "Email",
+        ),
+    ]
