0a8ebf531711890d738fce205aaab8c7972ee003
Mike Boldrick <mb@boldrick.net>
2025-06-16 16:53:24 -0700

    Configure AWS S3 storage settings in production environment

--- vagabond_site/settings/production.py
+++ vagabond_site/settings/production.py
@@ -1,6 +1,20 @@
+import os
 from .base import *
 
 DEBUG = False
+
+INSTALLED_APPS += ["storages"]
+
+APP_NAME = os.environ.get("FLY_APP_NAME")
+ALLOWED_HOSTS = [f"{APP_NAME}.fly.dev"]
+AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", "us-west-1")
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 try:
     from .local import *
