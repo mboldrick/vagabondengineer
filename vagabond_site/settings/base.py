66f103b94eace4f0f507fc2ac34f4d0479acab36
Mike Boldrick <mb@boldrick.net>
2025-06-17 11:34:17 -0700

    Add Whitenoise for static file management and update settings

--- vagabond_site/settings/base.py
+++ vagabond_site/settings/base.py
@@ -143,7 +143,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
--- vagabond_site/settings/production.py
+++ vagabond_site/settings/production.py
@@ -9,16 +9,22 @@ CSRF_TRUSTED_ORIGINS = os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS", "").split("
 
 INSTALLED_APPS += ["storages"]
 
-APP_NAME = os.environ.get("FLY_APP_NAME")
-ALLOWED_HOSTS = [f"{APP_NAME}.fly.dev"]
+# APP_NAME = os.environ.get("FLY_APP_NAME")
+# ALLOWED_HOSTS = [f"{APP_NAME}.fly.dev"]
+SECRET_KEY = os.environ["SECRET_KEY"]
 AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
 AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
 AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", "us-west-1")
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
+MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    *MIDDLEWARE,  # keep your other middleware
+]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 # Postgres settings
 DATABASES = {"default": dj_database_url.config(conn_max_age=600)}
